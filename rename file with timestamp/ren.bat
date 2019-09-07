dbf9cae39ce9c3e2dbfcb6388c4fb05a4875a45f
AlexCeng <izengjing.cn@gmail.com>
2019-09-07 10:29:10 +0800

    [update]: update time formate

--- rename file with timestamp/ren.bat
+++ rename file with timestamp/ren.bat
@@ -1,20 +1,20 @@
 @echo off
 
 set YYYYmmdd=%date:~0,4%%date:~5,2%%date:~8,2%
-set hhmiss=%time:~0,2%%time:~3,2%%time:~6,2%
+if "%time:~0,2%" lss "10" (set hhmiss=0%time:~1,1%%time:~3,2%%time:~6,2%) else (set hhmiss=%time:~0,2%%time:~3,2%%time:~6,2%)
 
 
 dir/ad %~n1 >nul 2>nul&&(goto bFold)||(goto bFile)
 
 
 :bFold
-set "fold_name=%YYYYmmdd% %~n1"
+set "fold_name=%YYYYmmdd%_%hhmiss% %~n1"
 ren %~n1 "%fold_name%"
 exit
 
 
 :bFile
-set "file_name=%YYYYmmdd% %~n1%~x1"
+set "file_name=%YYYYmmdd%_%hhmiss% %~n1%~x1"
 ren %~nx1 "%file_name%"
 exit
 
